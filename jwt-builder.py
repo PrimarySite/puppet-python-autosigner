a16c77b8d418fe34536df37e5a971adbefd77e21
Richard Paul <richard@primarysite.net>
2020-05-21 11:59:56 +0100

    Add Linux compatibility (just Ubuntu for now), still support FreeBSD by default.

--- jwt-builder.py
+++ jwt-builder.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 
-import requests
 import socket
+
+import requests
 import yaml
+try:
+    from distro import name
+except ModuleNotFoundError as e:
+    pass
 
 AUDIENCE_URL = 'http://{}'.format(socket.getfqdn())
 METADATA_HEADERS = {'Metadata-Flavor': 'Google'}
@@ -12,6 +17,13 @@ url = 'http://metadata.google.internal/computeMetadata/v1/instance/service-accou
 r = requests.get(url, headers=METADATA_HEADERS)
 csr_attributes = dict(custom_attributes = dict(challengePassword = r.text))
 
-with open('/usr/local/etc/puppet/csr_attributes.yaml', 'w') as f:
-    yaml.dump(csr_attributes, f, default_flow_style=False)
+puppet_dir = '/usr/local/etc/puppet'
+try:
+    linux_distros = ['ubuntu']
+    if str(name()).lower() in linux_distros:
+        puppet_dir = '/etc/puppet'
+except NameError as e:
+    pass
 
+with open(f'{puppet_dir}/csr_attributes.yaml', 'w') as f:
+    yaml.dump(csr_attributes, f, default_flow_style=False)
