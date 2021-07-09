141dbd4a5abbfe8cc174e3e9b4af7db8321d6903
Oliver Mussell <oliver.mussell@primarysite.net>
2021-07-09 14:42:18 +0100

    Initial refactoring

--- README.md
+++ README.md
@@ -6,3 +6,13 @@ py36-requests
 py36-google-auth
 
 This has to be run stand alone as an executable as puppet runs just the first path that it finds after the `autosigner = `
+
+
+
+The autosigner.py defaults to checking for instance certs created with jwt-builder.py. Then if its a jail it subprocesses to the autosign gem https://github.com/danieldreier/autosign. 
+
+You can send the jwt request to the google metadata endpoint from inside the jail too. So why dont we just get rid of the gem? Download jwt-builder and run it. Then it'll be the same process for both instances and jails.
+
+If we want to use this autosigner on the infra for other Juniper orgs, we probably want to make this more flexible too.
+
+We also need to change the jwt-builder to work with tests. This is an important part of the infra and needs tests.
---
+++ src/jwt-builder.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+# Standard Library
+import socket
+
+# 3rd-party
+import httpx
+import yaml
+
+try:
+    # 3rd-party
+    from distro import name
+except ModuleNotFoundError:
+    pass
+
+AUDIENCE_URL = f"http://{socket.getfqdn()}"
+METADATA_HEADERS = {"Metadata-Flavor": "Google"}
+URL = f"http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/identity?audience={AUDIENCE_URL}&format=full&licenses=TRUE"
+
+
+def query_google_jwt():
+    with httpx.Client() as client:
+        response = client.get(URL, headers=METADATA_HEADERS)
+
+    jwt_response = dict(custom_attributes=dict(challengePassword=response.text))
+
+    return jwt_response
+
+
+def get_os_etc():
+    # FreeBSD default
+    os_etc = "/usr/local/etc/puppet"
+
+    # Ubuntu
+    try:
+        linux_distros = ["ubuntu"]
+        if str(name()).lower() in linux_distros:
+            os_etc = "/etc/puppetlabs/puppet"
+    except NameError:
+        pass
+
+    return os_etc
+
+
+def dump_jwt_to_file(jwt_response, os_type):
+    with open(f"{puppet_dir}/csr_attributes.yaml", "w") as csr_attributes_file:
+        yaml.dump(jwt_response, csr_attributes_file, default_flow_style=False)
+
+
+def jwt_builder():
+    jwt_response = query_google_jwt()
+    os_etc = get_os_etc()
+    dump_jwt_to_file(jwt_response, os_etc)
+
+
+if __name__ == "__main__":
+    jwt_builder()
